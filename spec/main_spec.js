0e09081aa7c415e71e69735c8c7a2aed2f9c4d00
Shaneil Clarke <shaneilske@yahoo.com>
2019-01-24 16:57:41 +0000

    updated sample tests

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -4,13 +4,19 @@ describe('Square',()=>{
       let result = square1.area()
       expect(result).toEq(36)
     });
+
     can('throw error if argument is not a number', ()=> {
       square = function() { new Square('notanumber')}
-      expect(function(){new Square('notanumber')}).toThrowError('argument is not a number')
+      expect(square).toThrowError('argument is not a number')
     });
+
+    can('throw error if argument is less than 0', ()=> {
+      square1 = function() { new Square(-2)}
+      expect(square1).toThrowError('size should be a positive number')
+    });
+
     can('know its size',()=>{
       let square2 = new Square(7)
       expect(square2.size).toEq(7)
     });
-
-})
+});
